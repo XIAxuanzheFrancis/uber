453d6b42c0d292264132ebefdcc6fe3dc80167e1
Xuanzhe XIA <xuanzhe.xia@etu.uca.fr>
2024-02-04 13:31:13 +0100

    paging search

--- src/main/java/com/xuanzhe/controller/EmployeeController.java
+++ src/main/java/com/xuanzhe/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.xuanzhe.controller;
 
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.conditions.query.LambdaQueryChainWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.xuanzhe.common.R;
 import com.xuanzhe.pojo.Employee;
 import com.xuanzhe.service.EmployeeService;
@@ -9,12 +10,10 @@ import com.xuanzhe.service.impl.EmployeeServiceImpl;
 import java.time.LocalDateTime;
 import javax.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.DigestUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -61,6 +60,20 @@ public class EmployeeController {
     employee.setUpdateUser(empId);
     employeeService.save(employee);
     return R.success("Add Employee Success");
+  }
+
+  @GetMapping("/page")
+  public R<Page> page(int page, int pageSize, String name){
+    log.info("page={}, pageSize={}, name={}",page,pageSize,name);
+    Page pageInfo = new Page(page, pageSize);
+
+    LambdaQueryWrapper<Employee> lambdaQueryWrapper = new LambdaQueryWrapper();
+    lambdaQueryWrapper.like(StringUtils.isNotEmpty(name), Employee::getName, name);
+
+    lambdaQueryWrapper.orderByDesc(Employee::getUpdateTime);
+
+    employeeService.page(pageInfo, lambdaQueryWrapper);
+    return R.success(pageInfo);
   }
 
 }
